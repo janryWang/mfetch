2c2a7c8f3402aaf4a58364c987c5fbae296e0d29
zhili.wzl <wangzhili56@126.com>
2018-06-15 11:19:36 +0800

    bugfix with uritemplate decodeUri

--- src/plugins/url.js
+++ src/plugins/url.js
@@ -10,7 +10,12 @@ export const url = (url) => ({
         options = previous(options)
         options.url = url
         if (options.uriTemplate) {
-            options.uri = new ut(String(url))
+            if(url instanceof URL){
+                url = url.href
+            } else {
+                url = String(url || "")
+            }
+            options.uri = new ut(String(decodeURIComponent(url)))
             delete options.uriTemplate
         }
         return options
